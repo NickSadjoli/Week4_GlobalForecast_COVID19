7bb55dd73932ba29e3f36f81f3a8c9b87b6d4d4d
NickSadjoli <nick.sadjoli@gmail.com>
2020-03-30 05:29:08 +0800

    Second version of webscraper that now can also scrape the chart data from Worldometer main page. Next TODO: Feature for scraper to collect timeseries and (optionally) regional data from all countries with Worldometer pages

--- tools/worldometer_scraper.py
+++ tools/worldometer_scraper.py
@@ -2,26 +2,201 @@ from selenium import webdriver
 import pandas as pd
 from bs4 import BeautifulSoup
 import sys
+import re
 
 worldometer_path = "https://www.worldometers.info/coronavirus/"
 worldometer_country_path = worldometer_path + "/country/"
 
-class MainDataLatest():
+## TODO: Create a Class that can scrape the time series data for all countries that has hrefs in Worldometer!
+class TimeSeries_by_Country():
+    def __init__(self):
+        return
+
+#For consideration: Separate Time Series as a separate class?
+class GlobalTimeSeries():
+    def __init__(self, mainpage_soup=None, driver=None, verbose=False):
+        
+        self.driver = self._check_webdriver(driver=driver, verbose=verbose)
+
+        if mainpage_soup is not None:
+            self.mainpage_soup = mainpage_soup
+        else:
+            self._check_webdriver(driver)
+            self.mainpage_soup = BeautifulSoup(self.driver.page_source, "html.parser")
+        
+        #find all dom-s with charts created with Highchart.js 
+        self.global_charts = mainpage_soup.body.find_all('script', text=re.compile("Highcharts.chart"))
+        self.global_timeseries_dict = {}
+        
+        self.domId_map = {'total-currently-infected-linear': 'Current Active Cases (Linear)',
+                          'deaths-cured-outcome-small': 'Closed Cases',
+                          'coronavirus-cases-linear': 'Cumulative Confirmed (Linear)',
+                          'coronavirus-cases-log': 'Cumulative Confirmed (Logarithmic)',
+                          'coronavirus-deaths-linear': 'Cumulative Deaths (Linear)',
+                          'coronavirus-deaths-log': 'Cumulative Deaths (Logarithmic)'
+                          }
+
+        self.parse_global_timeseries(verbose=verbose)
+
+        if verbose:
+            print(self.global_timeseries_data)
+
+
+
+    def _check_webdriver(self, driver, verbose=False):
+        if driver is not None:
+            if verbose:
+                print("driver is already initiated")
+            return driver
+        else:
+            if verbose:
+                print("no drivers initated yet")
+            cur_driver = webdriver.Chrome()
+            cur_driver.get(worldometer_path)
+            return cur_driver
+
+    def parse_global_timeseries(self, verbose=False):
+        js_charts = self.global_charts
+
+        for i in range(0, len(js_charts)):
+            list_of_charts = self.find_all_highcharts(js_charts[i].text, verbose)
+
+            for cur_chart_domId in list_of_charts:
+                cur_chartData = list_of_charts[cur_chart_domId]
+                dates = cur_chartData.split("categories: [")[1].split("]")[0].replace('\"', '')
+                dates = dates.split(',')
+
+                #there are potentially more than just one data series projected in one chart (e.g. Closed Cases)
+                series_data = cur_chartData.split("series: [{")[1]
+                values_dict = self.find_all_values(series_data, verbose)
+
+                if len(self.global_timeseries_dict) == 0:
+                    self.global_timeseries_dict['dates'] = dates #All WorldoMeter Time series charts assumed to share the same dates
+
+                if len(values_dict) > 1:
+                    for val_name in values_dict:
+                        self.global_timeseries_dict[cur_chart_domId + " - " + val_name] = values_dict[val_name]
+                else:
+                    for val_name in values_dict:
+                        self.global_timeseries_dict[cur_chart_domId] = values_dict[val_name]
+
+        ts_data_col = self.global_timeseries_dict['dates']
+        ts_data_col.insert(0, 'Data Type')
+        print("number of columns", len(ts_data_col))
+
+        #not including dates as a main data row
+        self.global_timeseries_data = pd.DataFrame(index=range(0, len(self.global_timeseries_dict) -1), columns=ts_data_col) 
+
+        i = 0
+        #for i in range(0, len(self.global_timeseries_dict) -1):
+        for data_type in self.global_timeseries_dict:
+            if data_type == "dates":
+                continue
+                
+            to_be_inserted = self.global_timeseries_dict[data_type]
+
+            #Some data might be started to be recorded at a different date. In this case fill the rest of the data with '0'
+            if len(to_be_inserted) < len(ts_data_col):
+                for j in range(0, len(ts_data_col) - len(to_be_inserted) - 1):
+                    to_be_inserted.insert(0, '0')
+            to_be_inserted.insert(0, data_type)
+            self.global_timeseries_data.loc[i] = to_be_inserted #self.global_timeseries_dict[data_type]
+            i += 1
+
+        return
+
+    def find_all_highcharts(self, js_text, verbose=False):
+
+        listed_charts = {}
+
+        def find_highchart_recur(js_text, listed_charts, verbose=False):
+            
+            if verbose:
+                print(js_text.count("Highcharts.chart("))
+            
+            if js_text.count("Highcharts.chart(") <= 1:
+                cur_domId = js_text.split("Highcharts.chart(" )[1].split("'")[1]
+                previous_data, cur_chartData = js_text.split("Highcharts.chart('" + cur_domId + "',")
+                listed_charts[self.domId_map[cur_domId]] = cur_chartData
+                return previous_data, listed_charts
+            else:
+                cur_domId = js_text.split("Highcharts.chart(")[1].split("'")[1]
+                subsequent_strings = js_text.split("Highcharts.chart('" + cur_domId + "',")[1]
+                cur_chartData, listed_charts = find_highchart_recur(subsequent_strings, listed_charts)
+                listed_charts[self.domId_map[cur_domId]] = cur_chartData
+                return cur_chartData, listed_charts
+
+        _, listed_charts = find_highchart_recur(js_text, listed_charts, verbose)
+        return listed_charts
+
+    def find_all_values(self, series_data, verbose=False):
+
+        values_dict = {}
+        '''
+        def find_values_recur():
+            if series_data.count("name:") <=1:
+
+            return
+        '''
+        def find_values_recur(series_data, values_dict, verbose=False):
+            
+            if verbose:
+                print(series_data.count("name: '"))
+
+            val_name = series_data.split("name: '")[1].split("',")[0]
+            values_str = series_data.split("data: [")[1].split("]")[0].replace('\"', '')
+            values_str = series_data.split("data: [")[1].split("]")[0].replace('\"', '')
+            values = values_str.split(',')
+            values_dict[val_name] = values
+
+            if series_data.count("name: '") <= 1:
+                return values_dict
+            else:
+                next_vals = series_data.split("data: [" + values_str + "]")[1]#.split("]")[1]
+                values_dict = find_values_recur(next_vals, values_dict=values_dict)
+                return values_dict
+
+        return find_values_recur(series_data, values_dict, verbose)
+
+class GlobalCasesLatest():
     '''
     Class that scrapes the main page of the Worldometer site.
     '''
-    def __init__(self, verbose=False):
-        self.driver = webdriver.Chrome()
-        self.driver.get(worldometer_path)
-        self.mainpage_data = BeautifulSoup(self.driver.page_source, "html.parser")
+    def __init__(self, mainpage_soup=None, driver=None, verbose=False):
+        
+        self.driver = self._check_webdriver(driver=driver, verbose=verbose)
 
-        self.latest_table = self.mainpage_data.find_all('table')[0]
+        if mainpage_soup is not None:
+            self.mainpage_soup = mainpage_soup
+        else:
+            self.mainpage_soup = BeautifulSoup(self.driver.page_source, "html.parser")
+
+        self.mainpage_soup = BeautifulSoup(self.driver.page_source, "html.parser")
+
+        #Get the Main Table first
+        self.latest_table = self.mainpage_soup.find_all('table')[0]
         self.latest_table_data = None
         self.country_list = []
         self.countries_w_href = {}
         self.main_columns = None
         self.num_of_columns = 0
-        self.parse_latest_main()
+        self.parse_latest_maintable()
+
+        #Then get the Charts
+        #find all dom-s with charts created with Highchart.js 
+        self.global_charts = mainpage_soup.body.find_all('script', text=re.compile("Highcharts.chart"))
+        self.global_timeseries_dict = {}
+        
+        self.domId_map = {'total-currently-infected-linear': 'Current Active Cases (Linear)',
+                          'deaths-cured-outcome-small': 'Closed Cases',
+                          'coronavirus-cases-linear': 'Cumulative Confirmed (Linear)',
+                          'coronavirus-cases-log': 'Cumulative Confirmed (Logarithmic)',
+                          'coronavirus-deaths-linear': 'Cumulative Deaths (Linear)',
+                          'coronavirus-deaths-log': 'Cumulative Deaths (Logarithmic)'
+                          }
+                          
+        self.global_timeseries_data = None
+        self.parse_global_timeseries(verbose=verbose)
 
         if verbose:
             print("country lists")
@@ -29,8 +204,22 @@ class MainDataLatest():
             print(self.countries_w_href)
             print("list of columns", self.main_columns, self.num_of_columns)
             print(self.latest_table_data)
+            print(self.global_timeseries_data)
+    
+    def _check_webdriver(self, driver, verbose=False):
+        if driver is not None:
+            if verbose:
+                print("driver is already initiated")
+            return driver
+        else:
+            if verbose:
+                print("no drivers initated yet")
+            cur_driver = webdriver.Chrome()
+            cur_driver.get(worldometer_path)
+            return cur_driver
 
-    def parse_latest_main(self):
+    #### Functions for Scraping the Table from Main Coronavirus Page ###
+    def parse_latest_maintable(self):
         rows = self.latest_table.find_all('tr')
         header_row = rows[0].find_all('th')
         column_names = [head.text.replace('\xa0', ' ') for head in header_row] #&nbsp gets turned into \xa0 by BeautifulSoup
@@ -63,10 +252,6 @@ class MainDataLatest():
     def get_list_of_countries_w_href(self):
         return self.countries_w_href
 
-    def get_num_countries(self):
-        self.country_len = len(self.web_table.find_elements_by_xpath("./tr"))
-        return self.country_len
-
     def get_latestTable_data(self):
         return self.latest_table_data
 
@@ -79,10 +264,140 @@ class MainDataLatest():
         self.latest_table_data.to_csv(path, sep=',')       
         return
 
-class TimeSeriesData(MainDataLatest):
-    def __init__(self, verbose=False):
-        self.driver = None
+    ####################################################################
+
+
+
+    #### Functions for Scraping the Charts from Main Coronavirus Page ###
+    def parse_global_timeseries(self, verbose=False):
+        js_charts = self.global_charts
+
+        for i in range(0, len(js_charts)):
+
+            #There might be more than 1 chart in one particular dom
+            list_of_charts = self.find_all_highcharts(js_charts[i].text, verbose)
+
+            for cur_chart_domId in list_of_charts:
+                cur_chartData = list_of_charts[cur_chart_domId]
+                dates = cur_chartData.split("categories: [")[1].split("]")[0].replace('\"', '')
+                dates = dates.split(',')
+
+                #there are potentially more than just one data series projected in one chart (e.g. Closed Cases)
+                series_data = cur_chartData.split("series: [{")[1]
+                values_dict = self.find_all_values(series_data, verbose)
+
+                if len(self.global_timeseries_dict) == 0:
+                    self.global_timeseries_dict['dates'] = dates #All WorldoMeter Time series charts assumed to share the same dates
+
+                if len(values_dict) > 1:
+                    for val_name in values_dict:
+                        self.global_timeseries_dict[cur_chart_domId + " - " + val_name] = values_dict[val_name]
+                else:
+                    for val_name in values_dict:
+                        self.global_timeseries_dict[cur_chart_domId] = values_dict[val_name]
+
+        ts_data_col = self.global_timeseries_dict['dates']
+        ts_data_col.insert(0, 'Data Type')
+        print("number of columns", len(ts_data_col))
+
+        #not including dates as a main data row
+        self.global_timeseries_data = pd.DataFrame(index=range(0, len(self.global_timeseries_dict) -1), columns=ts_data_col) 
+
+        i = 0
+        #for i in range(0, len(self.global_timeseries_dict) -1):
+        for data_type in self.global_timeseries_dict:
+            if data_type == "dates":
+                continue
+                
+            to_be_inserted = self.global_timeseries_dict[data_type]
+
+            #Some data might be started to be recorded at a different date. In this case fill the rest of the data with '0'
+            if len(to_be_inserted) < len(ts_data_col):
+                for j in range(0, len(ts_data_col) - len(to_be_inserted) - 1):
+                    to_be_inserted.insert(0, '0')
+            to_be_inserted.insert(0, data_type)
+            self.global_timeseries_data.loc[i] = to_be_inserted #self.global_timeseries_dict[data_type]
+            i += 1
+
+        return
+
+    def find_all_highcharts(self, js_text, verbose=False):
+
+        listed_charts = {}
+
+        def find_highchart_recur(js_text, listed_charts, verbose=False):
+            
+            if verbose:
+                print(js_text.count("Highcharts.chart("))
+            
+            if js_text.count("Highcharts.chart(") <= 1:
+                cur_domId = js_text.split("Highcharts.chart(" )[1].split("'")[1]
+                previous_data, cur_chartData = js_text.split("Highcharts.chart('" + cur_domId + "',")
+                listed_charts[self.domId_map[cur_domId]] = cur_chartData
+                #listed_charts[cur_domId] = cur_chartData
+                return previous_data, listed_charts
+            else:
+                cur_domId = js_text.split("Highcharts.chart(")[1].split("'")[1]
+                subsequent_strings = js_text.split("Highcharts.chart('" + cur_domId + "',")[1]
+                cur_chartData, listed_charts = find_highchart_recur(subsequent_strings, listed_charts)
+                listed_charts[self.domId_map[cur_domId]] = cur_chartData
+                return cur_chartData, listed_charts
+
+        _, listed_charts = find_highchart_recur(js_text, listed_charts, verbose)
+        return listed_charts
+
+    def find_all_values(self, series_data, verbose=False):
+
+        values_dict = {}
+        '''
+        def find_values_recur():
+            if series_data.count("name:") <=1:
+
+            return
+        '''
+        def find_values_recur(series_data, values_dict, verbose=False):
+            
+            if verbose:
+                print(series_data.count("name: '"))
+
+            val_name = series_data.split("name: '")[1].split("',")[0]
+            values_str = series_data.split("data: [")[1].split("]")[0].replace('\"', '')
+            values_str = series_data.split("data: [")[1].split("]")[0].replace('\"', '')
+            values = values_str.split(',')
+            values_dict[val_name] = values
+
+            if series_data.count("name: '") <= 1:
+                return values_dict
+            else:
+                next_vals = series_data.split("data: [" + values_str + "]")[1]#.split("]")[1]
+                values_dict = find_values_recur(next_vals, values_dict=values_dict)
+                return values_dict
+
+        return find_values_recur(series_data, values_dict, verbose)
+    
+    def get_globalTimeSeries(self):
+        return self.global_timeseries_data
+
+    def write_globalTimeSeries_to_csv(self, path=None):
+        if path is None:
+            path = "./Main_Worldometer_TimeSeries.csv"
+        self.global_timeseries_data.to_csv(path, sep=',', float_format='%.5f')       
+        return
+    #####################################################################
+
+
 
 if __name__=="__main__":
-    main_table = MainDataLatest(verbose=True)
-    main_table.write_latestTable_to_csv("./data/Main_Worldometer_Table.csv")
+    
+    driver = webdriver.Chrome()
+    driver.get(worldometer_path)
+    mainpage_soup = BeautifulSoup(driver.page_source, "html.parser")
+
+    global_cases_data = GlobalCasesLatest(mainpage_soup=mainpage_soup, driver=driver, verbose=True)
+    global_cases_data.write_latestTable_to_csv("./data/Main_Worldometer_Table.csv")
+    global_cases_data.write_globalTimeSeries_to_csv("./data/Main_Worldometer_TimeSeries.csv")
+
+    #global_timeseries = GlobalTimeSeries(mainpage_soup=mainpage_soup, driver=driver, verbose=True)
+
+
+
