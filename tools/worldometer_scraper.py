571bf5f5db34ebc2b83ed81269594e844ef60eab
NickSadjoli <nick.sadjoli@gmail.com>
2020-03-30 00:56:53 +0800

    Cleaned up first version of Worldometer Scraper to remove redundant lines of code using Selenium (replaced with BeautifulSoup). Also included .gitignore and example csv data from WebScraping. Next TODO: Try to have scraper tool to also scrape data from other per country pages, as well as the graph data (not just table)

--- tools/worldometer_scraper.py
+++ tools/worldometer_scraper.py
@@ -16,7 +16,7 @@ class MainDataLatest():
         self.mainpage_data = BeautifulSoup(self.driver.page_source, "html.parser")
 
         self.latest_table = self.mainpage_data.find_all('table')[0]
-        self.table_data = None
+        self.latest_table_data = None
         self.country_list = []
         self.countries_w_href = {}
         self.main_columns = None
@@ -28,39 +28,15 @@ class MainDataLatest():
             print(self.country_list)
             print(self.countries_w_href)
             print("list of columns", self.main_columns, self.num_of_columns)
-            print(self.table_data)
+            print(self.latest_table_data)
 
-        sys.exit(0)
-        
-        #If using purely selenium. Not recommended as BeautifulSoup is much faster and cleaner
-        self.web_table = self.driver.find_element_by_xpath('//*[@id="main_table_countries_today"]/tbody[1]')
-        self.web_table_header = self.driver.find_element_by_xpath('//*[@id="main_table_countries_today"]/thead')
-        self.table_columns = []
-        self.num_of_columns = 0
-        self.get_table_columns()
-        self.country_len = 0
-        self.get_num_countries()
-        self.country_list = []
-        self.countries_w_href = {}
-        self.get_country_list(verbose)
-
-        self.table_data = pd.DataFrame(index=range(0, self.country_len), columns=self.table_columns)
-        self.update_table_data()
-        
-        if verbose:
-            print("country lists")
-            print(self.country_list)
-            print(self.countries_w_href)
-            print("list of columns", self.table_columns, self.num_of_columns)
-            print(self.table_data)
-        
     def parse_latest_main(self):
         rows = self.latest_table.find_all('tr')
         header_row = rows[0].find_all('th')
         column_names = [head.text.replace('\xa0', ' ') for head in header_row] #&nbsp gets turned into \xa0 by BeautifulSoup
         self.main_columns = column_names
         self.num_of_columns = len(column_names)
-        self.table_data = pd.DataFrame(index=range(0, len(rows[1:])), columns=column_names)
+        self.latest_table_data = pd.DataFrame(index=range(0, len(rows[1:])), columns=column_names)
 
         #HTML seems to use the 1-indexing system instead of the normal 0-indexing system
         for i in range(1, len(rows)):
@@ -72,70 +48,35 @@ class MainDataLatest():
             if len(href_check) > 0:
                 self.countries_w_href[row_elements[0].text] = worldometer_path + href_check[0]['href']
 
-            self.table_data.loc[i-1] = row_values
+            self.latest_table_data.loc[i-1] = row_values
         return
 
-    def get_table_columns(self):
-        column_els = self.web_table_header.find_elements_by_xpath("./tr/th")
-        table_columns = []
-        for el in column_els:
-            table_columns.append(el.text.replace('\n', ' '))
-        self.table_columns = table_columns
-        self.num_of_columns = len(table_columns)
-        return table_columns
+    def get_latestTable_columns(self):
+        return self.main_columns
+
+    def get_list_of_countries(self):
+        return self.country_list
+
+    def get_number_of_countries(self):
+        return len(self.country_list) - 1 #Not counting the 'World' as a country
+    
+    def get_list_of_countries_w_href(self):
+        return self.countries_w_href
 
     def get_num_countries(self):
         self.country_len = len(self.web_table.find_elements_by_xpath("./tr"))
         return self.country_len
 
-    def get_country_list(self, verbose=False):
-        country_list = []
-        countries_w_href = {}
-        country_href = None
-
-        for i in range(1, self.country_len + 1):
-            try:
-                cur_element = self.web_table.find_element_by_xpath("./tr[{}]/td[1]/a".format(str(i)))
-                country_href = cur_element.get_attribute("href")
-                country_name = cur_element.text
-                country_name = country_name.lower()
-                countries_w_href[country_name] = country_href
-                #countries_w_href.append(cur_element.text.lower())
-            except:
-                cur_element = self.web_table.find_element_by_xpath("./tr[{}]/td[1]".format(str(i)))
-            cur_country = cur_element.text
-            if verbose:
-                print("Country #{}=>".format(str(i-1)), cur_country) 
-            country_list.append(cur_country)
-
-        self.country_list = country_list
-        self.countries_w_href = countries_w_href
-
-        return country_list, countries_w_href
-
-    def update_table_data(self):
-        table_data = self.table_data
-        for i in range(1, self.country_len + 1):
-            row_elements = self.web_table.find_elements_by_xpath("./tr[{}]/".format(str(i)))
-            row_values = [el.text for el in row_elements[1:]]
-            #print(row_elements[1:].text)
-            #table_data.loc[i] = row_values
-            print(row_values, len(row_values), row_elements[1].text)
-            table_data.loc[i][self.table_columns[0]] = self.country_list[i-1]
-            table_data.loc[i][self.table_columns[1]:] = row_values
-
-        return
-
-    def get_MainTable(self):
-        return self.table_data
+    def get_latestTable_data(self):
+        return self.latest_table_data
 
     def get_countries_w_href(self):
         return self.countries_w_href
 
-    def write_to_csv(self, path=None):
+    def write_latestTable_to_csv(self, path=None):
         if path is None:
             path = "./Main_Worldometer_Table.csv"
-        self.table_data.to_csv(path, sep=',')       
+        self.latest_table_data.to_csv(path, sep=',')       
         return
 
 class TimeSeriesData(MainDataLatest):
@@ -144,4 +85,4 @@ class TimeSeriesData(MainDataLatest):
 
 if __name__=="__main__":
     main_table = MainDataLatest(verbose=True)
-    #main_table.write_to_csv()
+    main_table.write_latestTable_to_csv("./data/Main_Worldometer_Table.csv")
